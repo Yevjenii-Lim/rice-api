f54cfe36e10b270a2ff5b400dea8c10ad198c08f
yevheniilim <pacmanlim27@gmail.com>
2021-06-11 14:42:28 +0300

    add data bse

--- app.py
+++ app.py
@@ -19,7 +19,11 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api.add_resource(Items, "/")
 api.add_resource(ItemsAll, "/all")
 
-
+# @app.before_first_request #delet this block before posting to heroku
+# def create_tables():
+    # print("tables created")
+    # db.create_all()
 
 if __name__ == '__main__':
+    # db.init_app(app)  #delete this before posting to heroku
     app.run(port="5000", debug=True)
