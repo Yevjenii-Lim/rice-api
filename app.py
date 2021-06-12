b56c77414066878f202e3969b8b8b956d77d66eb
yevheniilim <pacmanlim27@gmail.com>
2021-06-12 10:23:12 +0300

    cors plugins

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ import datetime
 import os
 import sqlite3
 from db import db
+import sys
+from flask_cors import CORS
 
 
+print(sys.path)
 app = Flask(__name__)
-
+CORS(app)
 api = Api(app)
 
 app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(seconds=1800)
@@ -21,8 +24,8 @@ api.add_resource(ItemsAll, "/all")
 
 # @app.before_first_request #delet this block before posting to heroku
 # def create_tables():
-    # print("tables created")
-    # db.create_all()
+#     print("tables created")
+#     db.create_all()
 
 if __name__ == '__main__':
     # db.init_app(app)  #delete this before posting to heroku
