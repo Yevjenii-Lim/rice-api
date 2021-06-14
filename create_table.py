73d7d3b229d2c69b9a374ccb340dc0909370810a
yevheniilim <pacmanlim27@gmail.com>
2021-06-14 16:57:36 +0300

    new datab

--- create_table.py
+++ create_table.py
@@ -8,7 +8,7 @@ connection = sqlite3.connect("data.db")
 
 cursor = connection.cursor()
 
-create_items_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, title text, price real, desc text, type text, img text)"
+create_items_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, title text, price real, desc text, type text, img text, amount real)"
 
 
 cursor.execute(create_items_table)
@@ -33,7 +33,8 @@ def add_all():
               "desc": "Нежный сыр, огурец с мясом нежного краба...",
               "id": 1,
               "type": "roll",
-              "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/kali-crab_jrglhw.jpg"
+              "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/kali-crab_jrglhw.jpg",
+              "amount": 1
             },
             {
                "title": "Дакота",
@@ -41,7 +42,8 @@ def add_all():
                "desc": "Лосось, уsгорь, креветка, авокадо, яп майонез, икра тобико, икра лососьВес: 265 грамм.",
                "id": 2,
                "type": "roll",
-               "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491762/dakota_oth9ai.jpg"
+               "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491762/dakota_oth9ai.jpg",
+                 "amount": 1
             },
              {
                 "title": "Сет «Rice max»",
@@ -49,7 +51,8 @@ def add_all():
                 "id": 5,
                 "desc": "Акционная цена! Ролл Кокаин, Якудза, Оранж, сушими из лосося и тунца, Тигровый дракон, Икура и Дьябло",
                 "type": "set",
-                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/rice-max_tdph0c.jpg"
+                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/rice-max_tdph0c.jpg",
+                   "amount": 1
                 },
             {
                 "title": "Сет «ВЕГАН»",
@@ -57,7 +60,8 @@ def add_all():
                 "id": 6,
                 "desc":"Aкционная цена! маки с огурцом, маки с авокадо, Ясай-маки (болгарский перец, помидор, авокадо, огурец, такуан, салатный лист), vegan-Калифорния в кунжуте (авокадо, огурец, болгарский перец, такуан), гункан с чукой, 4 шт",
                 "type": "set",
-                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/rice-max_tdph0c.jpg"
+                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/rice-max_tdph0c.jpg",
+                  "amount": 1
             },
             {
                 "title": "Удон с курицей",
@@ -65,7 +69,8 @@ def add_all():
                 "id": 8,
                 "desc": "Лапша Удон, Куринное филе ,Соус якитори, Овощи....",
                 "type": "nudels",
-                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491764/udon-chiken_embvqd.jpg"
+                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491764/udon-chiken_embvqd.jpg",
+                   "amount": 1
 
             },
             {
@@ -74,7 +79,8 @@ def add_all():
                 "id": 9,
                 "desc":"Рис с курицей и тигровыми креветками.",
                 "type": "nudels",
-                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/rice-shrimp_it9tjl.jpg"
+                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/rice-shrimp_it9tjl.jpg",
+                   "amount": 1
             },
             {
                 "title": "«Мисо широ с угрем»",
@@ -82,7 +88,8 @@ def add_all():
                 "id": 13,
                 "desc":"рыбный бульен, мисо паста, угорь, гриб шиитаке, водросли вакаме Вес: 300грамм",
                 "type": "soup",
-                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/soup-miso_hjo8pk.jpg"
+                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/soup-miso_hjo8pk.jpg",
+                   "amount": 1
             },
             {
                 "title": "Суп «Том-Ям»",
@@ -90,16 +97,17 @@ def add_all():
                 "id": 14,
                 "desc":"рыбный бульен, кокосовое молоко, лосось, креветки, мидии, гриб шиитаке, соус ким-чи ,водросли вакаме. Вес: 300грамм",
                 "type": "soup",
-                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/soup-tom_nftw0g.jpg"
+                 "img": "https://res.cloudinary.com/rice-lim-ali/image/upload/v1623491763/soup-tom_nftw0g.jpg",
+                   "amount": 1
             },
         ]
 
-    query = "INSERT or REPLACE into items VALUES(?, ?, ?, ?, ?, ?)"
+    query = "INSERT or REPLACE into items VALUES(?, ?, ?, ?, ?, ?, ?)"
 
 
 
     for item in cart:
-        cursor.execute(query, (item["id"],item["title"], item["price"], item["desc"], item["type"], item["img"]))
+        cursor.execute(query, (item["id"],item["title"], item["price"], item["desc"], item["type"], item["img"], item["amount"]))
 
     connection.commit()
     connection.close()
